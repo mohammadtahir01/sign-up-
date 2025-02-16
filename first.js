0b39c274684b38df93152694a8436ae5b259cf9c
mohammadtahir01 <mdtahirhusain0121@gmail.com>
2025-02-16 23:30:35 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,13 @@
+let change=()=>{
+    let show =document.querySelector("#log-n");
+    show.style.color="red";
+    show.style.background ="white";
+     show.style.border="1px solid gray";
+}
+
+let change1=()=>{
+    let show =document.querySelector("#sig-n");
+    show.style.color="red";
+    show.style.background ="white";
+    show.style.border="1px solid gray";
+}

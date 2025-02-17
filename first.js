5f2887c8a15382a473ff23934edbc2f6ed947786
mohammadtahir01 <mdtahirhusain0121@gmail.com>
2025-02-17 22:45:31 +0530

    commit

--- first.js
+++ first.js
@@ -1,13 +1,15 @@
 let change=()=>{
-    let show =document.querySelector("#log-n");
-    show.style.color="red";
-    show.style.background ="white";
-     show.style.border="1px solid gray";
-}
+    let show = document.querySelector("#log-n");
+    let show1 = document.querySelector("#sig-n");
 
-let change1=()=>{
-    let show =document.querySelector("#sig-n");
-    show.style.color="red";
-    show.style.background ="white";
-    show.style.border="1px solid gray";
+    if(show == "gray"){
+        show.style.color="black";
+        show.style.background ="#6f2da8";
+    }
+    else if(show1 == "#6f2da8"){
+        show1.style.color="black";
+        show1.style.background ="gray";
+    }
+  
+    // show.style.border="1px solid gray";
 }

d01237f7a9de87be34dce9fc36cc5e5a8dfc15e8
mohammadtahir01 <mdtahirhusain0121@gmail.com>
2025-03-15 23:44:10 +0530

    commit

--- vanta.js
+++ vanta.js
@@ -13,18 +13,7 @@
     points: 20.00,
     maxDistance: 30.00,
     spacing: 20.00
-  }),
-
-
- VANTA.CLOUDS({
-    el: "#id2",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    skyColor: 0x2222cf,
-    cloudColor: 0x69a3ed,
-    speed: 0.20
   })
+
+
  
---
+++ vanta2.js
@@ -0,0 +1,12 @@
+VANTA.CLOUDS({
+    el: "#id2",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    skyColor: 0x2222cf,
+    cloudColor: 0x69a3ed,
+    speed: 0.20
+  })
+ 
